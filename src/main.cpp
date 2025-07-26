9f1d4c8a7dbd28da1f3355cc1ccb2ad6bb3ade80
Maxh <44454544+1-max-1@users.noreply.github.com>
2025-07-26 23:35:32 +1200

    Basic working code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include <Arduino.h>
 
+#define BATT_DIVIDER_EN PIN_PD1
+#define BATT_DIVIDER PIN_PD0
+
 void setup() {
 	pinMode(PIN_PC0, OUTPUT);
+	pinMode(BATT_DIVIDER_EN, OUTPUT);
+	digitalWrite(BATT_DIVIDER_EN, LOW);
+	pinMode(BATT_DIVIDER, INPUT);
+
+	Serial.begin(9600);
 }
 
 void loop() {
-	digitalWrite(PIN_PC0, HIGH);
+	digitalWrite(LED_BUILTIN, HIGH);
 	delay(1000);
-	digitalWrite(PIN_PC0, LOW);
-	delay(400);
+
+	digitalWrite(BATT_DIVIDER_EN, HIGH);
+	delay(10);
+	Serial.println(analogRead(BATT_DIVIDER));
+
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+
+	digitalWrite(BATT_DIVIDER_EN, LOW);
+	delay(10);
+	Serial.println(analogRead(BATT_DIVIDER));
+	Serial.println();
 }
