b1ea10f0014a2107d41b76ba35ae83cf13746bd9
Maxh <44454544+1-max-1@users.noreply.github.com>
2025-07-28 22:06:07 +1200

    Working voltage reading code

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,40 @@
 #include <Arduino.h>
 
 #define BATT_DIVIDER_EN PIN_PD1
-#define BATT_DIVIDER PIN_PD0
+#define BATT_DIVIDER PIN_PD2
 
 void setup() {
-	pinMode(PIN_PC0, OUTPUT);
 	pinMode(BATT_DIVIDER_EN, OUTPUT);
-	digitalWrite(BATT_DIVIDER_EN, LOW);
+	//digitalWrite(BATT_DIVIDER_EN, HIGH);
+	delay(100);
 	pinMode(BATT_DIVIDER, INPUT);
 
 	Serial.begin(9600);
+
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, HIGH);
+}
+
+float getVoltage() {
+	return (float)(analogRead(BATT_DIVIDER) * 2) * 2.998f / 1023.0f;
 }
 
 void loop() {
-	digitalWrite(LED_BUILTIN, HIGH);
+	/*digitalWrite(LED_BUILTIN, HIGH);
 	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);*/
 
 	digitalWrite(BATT_DIVIDER_EN, HIGH);
 	delay(10);
-	Serial.println(analogRead(BATT_DIVIDER));
+	Serial.println(getVoltage());
 
-	digitalWrite(LED_BUILTIN, LOW);
 	delay(1000);
 
 	digitalWrite(BATT_DIVIDER_EN, LOW);
 	delay(10);
-	Serial.println(analogRead(BATT_DIVIDER));
+	Serial.println(getVoltage());
 	Serial.println();
+
+	delay(1000);
 }
